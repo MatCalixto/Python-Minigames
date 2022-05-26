3b53d0271c38c4a472a2e5033261be020586ad15
Matheus Bione <mathenbiolis@gmail.com>
2022-05-26 17:58:52 -0300

    Váriáveis agora estão em snake_case para melhor legibilidade

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,5 @@
-playagain = 1
-while playagain == 1:
+play_again = 1
+while play_again == 1:
     print("Digite 1 para português e 2 para inglês")
     print("Type 1 for portuguese and 2 for english")
     language = int(
@@ -29,14 +29,14 @@ while playagain == 1:
         print("-Exemplos disso são a1, b2, b3, c2.")
         print("-Todas as regras usuais de Jogo da Velha se aplicam, divirtam-se!")
     if language == 2:
-        firstplayername = str(input("\nFirst player's name is: "))
+        first_player_name = str(input("\nFirst player's name is: "))
     else:
-        firstplayername = str(
+        first_player_name = str(
             input("\nO nome do primeiro jogador é: "))  # ask user name
     if language == 2:
-        secondplayername = str(input("\nSecond player's name is: "))
+        second_player_name = str(input("\nSecond player's name is: "))
     else:
-        secondplayername = str(input("\nO nome do segundo jogador é: "))
+        second_player_name = str(input("\nO nome do segundo jogador é: "))
     print()
     a1 = "  "  # define all positions
     a2 = "  "
@@ -47,24 +47,24 @@ while playagain == 1:
     c1 = "  "
     c2 = "  "
     c3 = "  "
-    numberofgames = 0
+    number_of_games = 0
     player = 0
-    while numberofgames < 9:  # while less than 9 games have been played
+    while number_of_games < 9:  # while less than 9 games have been played
         if player == 0:
             if language == 2:
                 # where will he put a cross
-                move = str(input(firstplayername + " (X)'s turn: "))
+                move = str(input(first_player_name + " (X)'s turn: "))
                 print()
             else:
-                move = str(input("É a vez de " + firstplayername + " (X): "))
+                move = str(input("É a vez de " + first_player_name + " (X): "))
                 print()
         else:
             if language == 2:
                 # where will he put a ball
-                move = str(input(secondplayername + " (O)'s turn: "))
+                move = str(input(second_player_name + " (O)'s turn: "))
                 print()
             else:
-                move = str(input("É a vez de " + secondplayername + " (O): "))
+                move = str(input("É a vez de " + second_player_name + " (O): "))
                 print()
         if move == "a1":  # find the square
             if a1 == " X" or a1 == " O":
@@ -73,7 +73,7 @@ while playagain == 1:
                     print("Invalid position, that square is already taken, try again")
                 else:  # used square
                     print("Posição já foi tomada, tente novamente")
-                numberofgames = numberofgames - 1
+                number_of_games = number_of_games - 1
                 if player == 0:
                     player = 1
                 else:
@@ -89,7 +89,7 @@ while playagain == 1:
                     print("Invalid position, that square is already taken, try again")
                 else:
                     print("Posição já foi tomada, tente novamente")
-                numberofgames = numberofgames - 1
+                number_of_games = number_of_games - 1
                 if player == 0:
                     player = 1
                 else:
@@ -105,7 +105,7 @@ while playagain == 1:
                     print("Invalid position, that square is already taken, try again")
                 else:
                     print("Posição já foi tomada, tente novamente")
-                numberofgames = numberofgames - 1
+                number_of_games = number_of_games - 1
                 if player == 0:
                     player = 1
                 else:
@@ -121,7 +121,7 @@ while playagain == 1:
                     print("Invalid position, that square is already taken, try again")
                 else:
                     print("Posição já foi tomada, tente novamente")
-                numberofgames = numberofgames - 1
+                number_of_games = number_of_games - 1
                 if player == 0:
                     player = 1
                 else:
@@ -137,7 +137,7 @@ while playagain == 1:
                     print("Invalid position, that square is already taken, try again")
                 else:
                     print("Posição já foi tomada, tente novamente")
-                numberofgames = numberofgames - 1
+                number_of_games = number_of_games - 1
                 if player == 0:
                     player = 1
                 else:
@@ -153,7 +153,7 @@ while playagain == 1:
                     print("Invalid position, that square is already taken, try again")
                 else:
                     print("Posição já foi tomada, tente novamente")
-                numberofgames = numberofgames - 1
+                number_of_games = number_of_games - 1
                 if player == 0:
                     player = 1
                 else:
@@ -169,7 +169,7 @@ while playagain == 1:
                     print("Invalid position, that square is already taken, try again")
                 else:
                     print("Posição já foi tomada, tente novamente")
-                numberofgames = numberofgames - 1
+                number_of_games = number_of_games - 1
                 if player == 0:
                     player = 1
                 else:
@@ -185,7 +185,7 @@ while playagain == 1:
                     print("Invalid position, that square is already taken, try again")
                 else:
                     print("Posição já foi tomada, tente novamente")
-                numberofgames = numberofgames - 1
+                number_of_games = number_of_games - 1
                 if player == 0:
                     player = 1
                 else:
@@ -201,7 +201,7 @@ while playagain == 1:
                     print("Invalid position, that square is already taken, try again")
                 else:
                     print("Posição já foi tomada, tente novamente")
-                numberofgames = numberofgames - 1
+                number_of_games = number_of_games - 1
                 if player == 0:
                     player = 1
                 else:
@@ -216,7 +216,7 @@ while playagain == 1:
                 print("Invalid position, try again")
             else:
                 print("Posição inválida, tente novamente")
-            numberofgames = numberofgames - 1
+            number_of_games = number_of_games - 1
             if player == 0:
                 player = 1
             else:
@@ -230,12 +230,12 @@ while playagain == 1:
                 a1 == " X" and b2 == " X" and c3 == " X" or
                 a3 == " X" and b2 == " X" and c1 == " X"):
             if language == 2:
-                print(firstplayername + " \nwon the game, congrats!")
+                print(first_player_name + " \nwon the game, congrats!")
                 print("")
             else:
-                print(firstplayername + " \nganhou o jogo, parabéns!")
+                print(first_player_name + " \nganhou o jogo, parabéns!")
                 print("")
-            numberofgames = 15
+            number_of_games = 15
         if (a1 == " O" and a2 == " O" and a3 == " O" or  # did player 2 win the game?
                 b1 == " O" and b2 == " O" and b3 == " O" or
                 c1 == " O" and c2 == " O" and c3 == " O" or
@@ -245,12 +245,12 @@ while playagain == 1:
                 a1 == " O" and b2 == " O" and c3 == " O" or
                 a3 == " O" and b2 == " O" and c1 == " O"):
             if language == 2:
-                print(secondplayername + " \nwon the game, congrats!")
+                print(second_player_name + " \nwon the game, congrats!")
                 print()
             else:
-                print(secondplayername + " \nganhou o jogo, parabéns!")
+                print(second_player_name + " \nganhou o jogo, parabéns!")
                 print()
-            numberofgames = 15
+            number_of_games = 15
         print("       1    2    3 ")
         print("   a ", a1, "|", a2, "|", a3, "")  # print game
         print("      ---|----|--- ")
@@ -262,9 +262,9 @@ while playagain == 1:
             player = 1
         else:
             player = 0
-        numberofgames = numberofgames + 1
+        number_of_games = number_of_games + 1
     else:
-        if numberofgames == 16:  # end of loop, either it is a draw or a player won
+        if number_of_games == 16:  # end of loop, either it is a draw or a player won
             if language == 2:
                 print("Game ended")
             else:
@@ -275,12 +275,12 @@ while playagain == 1:
             else:
                 print("O jogo acabou, foi empate!")
         print()
-        playagain = 0
+        play_again = 0
         if language == 2:
-            playagain = int(input("Type 1 if you want to play again: "))
+            play_again = int(input("Type 1 if you want to play again: "))
             print()
         else:
-            playagain = int(input("Digite 1 se quiser jogar novamente: "))
+            play_again = int(input("Digite 1 se quiser jogar novamente: "))
             print()
 else:
     if language == 2:
