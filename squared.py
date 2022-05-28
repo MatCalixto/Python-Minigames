8513a36dc24f9f38e9ec215549000206e8ef3ca8
Matheus Bione <mathenbiolis@gmail.com>
2022-05-28 18:04:28 +0000

    Substituidos print() por \n ou print()

--- squared.py
+++ squared.py
@@ -7,9 +7,9 @@ while playagain == 1:
     ptype = random.randint(1,2)
     number = random.randint(11,30)
     sol = 0
-    
+
     print("Answer the math questions:")
-    print("")
+    print()
 
     if ptype == 1:
         answer = int(input(str(number) + " squared equals: "))
@@ -18,15 +18,12 @@ while playagain == 1:
         answer = int(input("The square root of " + str(number**2) + " equals: "))
         sol = number
 
-    print("")
     if answer == sol:
-        print("Congrats, your answer was correct")
+        print("\nCongrats, your answer was correct")
     else:
-        print("Unfortunately the answer was " + str(sol) + ", try again")
-    print("")
+        print("\nUnfortunately the answer was " + str(sol) + ", try again")
 
     playagain = 0
-    playagain = int(input("Type 1 if you want to play again: "))
-    print("")
-    
-print("Game has ended")
+    playagain = int(input("\nType 1 if you want to play again: "))
+
+print("\nGame has ended")
