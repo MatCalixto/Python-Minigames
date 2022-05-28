0a540f12abf6f3db023d024629358291e3f44a33
Matheus Bione <mathenbiolis@gmail.com>
2022-05-28 19:10:33 +0000

    Nome das variáveis agora seguem o PEP8

--- squared.py
+++ squared.py
@@ -3,17 +3,17 @@ from random import randint
 
 while True:
 
-    ptype = randint(1, 2)
+    problem_type = randint(1, 2)
     number = randint(11, 30)
 
     print("Answer the math questions:\n")
 
-    solution = number ** (2 / ptype)
-    challenge_number = number ** ptype
+    solution = number ** (2 / problem_type)
+    challenge_number = number ** problem_type
 
-    if ptype == 1:
+    if problem_type == 1:
         print(f"{challenge_number} squared equals: ")
-    elif ptype == 2:
+    elif problem_type == 2:
         print((f"The square root of {challenge_number} equals: "))
     else:
         raise ValueError("Invalid problem type.")
@@ -25,9 +25,9 @@ while True:
     else:
         print(f"\nUnfortunately the answer was {solution}, try again")
 
-    playagain = int(input("\nType 1 if you want to play again: "))
+    play_again = int(input("\nType 1 if you want to play again: "))
     print("************************************************")
-    if playagain != 1:
+    if play_again != 1:
         break
 
 print("\nGame has ended")
